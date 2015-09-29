ffaaad233402068db4bc11af22585be978d10c5a
Gregory G. Putzel <gregputzel@gmail.com>
2015-09-29 14:46:07 -0400

    Added alphabet to left of canvas

--- decode.js
+++ decode.js
@@ -1,5 +1,15 @@
 var c = document.getElementById("pict");
 var ctx = c.getContext("2d");
+var w = c.width;
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,20,100);
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+//ctx.fillStyle = "#FF0000";
+//ctx.fillRect(0,0,20,100);
+ctx.font = "14px Arial";
+
+for (var i=0; i<alphabet.length; i++){
+    ctx.fillText(alphabet[i],10,20+20*i);
+}
+
+//console.log(alphabet);
